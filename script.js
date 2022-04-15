226ee7a17c70460b5352721d371cdd0bac7d1d92
ayaznoori <ayaz.noori15@gmail.com>
2022-04-15 23:02:27 +0530

    7 day forecast added

--- script.js
+++ script.js
@@ -37,6 +37,32 @@ function show(data){
     map.src=`https://www.google.com/maps/embed/v1/place?key=AIzaSyBM7xfZ6ekcrWhf79KIju8kGADdjeHRSp4&q=${data.name}`;
     document.querySelector("#main_data").append(name,min,max,currentt,wind,dataw,icon);
 
+    let url3=`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=hourly,minutely&appid=244a7ecf42f965a472d8b3f2300adfd1`;
+    fetch(url3).then((data1)=>{
+        return data1.json();
+    }).then((data1)=>{
+        let today = new Date();
+        let day = today.getDay();
+        day++;
+        let daylist = ["Sun","Mon","Tue","Wed ","Thu","Fri","Sat"];
+        let counter=["one","two","three","four","five","six","seven"]
+        let temp=[];
+       for(let i=0;i<7;i++){
+           if(day==7)
+           day=0;
+           temp.push(daylist[day]);
+           day++; 
+           var bag=document.createElement("div");
+           const icon=document.createElement("img");
+           icon.src=`http://openweathermap.org/img/wn/${data1.daily[i].weather[0].icon}.png`;
+           let value= document.createElement("div");
+           value.innerHTML= Math.floor(data1.daily[i].temp.min-273)+"<sup>o</sup>C";
+           let dayv= document.createElement("div");
+           dayv.innerText=temp[i];
+           document.querySelector(`#${counter[i]}`).innerText="";
+           document.querySelector(`#${counter[i]}`).append(dayv,icon,value);
+       }
+    })
 }
 
 navigator.geolocation.getCurrentPosition(success);
@@ -44,6 +70,9 @@ navigator.geolocation.getCurrentPosition(success);
 function success(pos){
     var crd = pos.coords;
   let url= `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=244a7ecf42f965a472d8b3f2300adfd1`;
+  
+  let url3=`https://api.openweathermap.org/data/2.5/onecall?lat=${crd.latitude}&lon=${crd.longitude}&exclude=hourly,minutely&appid=244a7ecf42f965a472d8b3f2300adfd1`;
+
   fetch(url)
  .then((data)=>{
    return data.json();
@@ -54,6 +83,36 @@ function success(pos){
 .catch((err)=>{
     console.log(err)
 })
+   fetch(url3).then((data1)=>{
+    return data1.json();
+}).then((data1)=>{
+    let today = new Date();
+    let day = today.getDay();
+    day++;
+    let daylist = ["Sun","Mon","Tue","Wed ","Thu","Fri","Sat"];
+    let counter=["one","two","three","four","five","six","seven"]
+    let temp=[];
+   for(let i=0;i<7;i++){
+       if(day==7)
+       day=0;
+       temp.push(daylist[day]);
+       day++; 
+       var bag=document.createElement("div");
+       const icon=document.createElement("img");
+       icon.src=`http://openweathermap.org/img/wn/${data1.daily[i].weather[0].icon}.png`;
+       let value= document.createElement("div");
+       value.innerHTML= Math.floor(data1.daily[i].temp.min-273)+"<sup>o</sup>C";
+       let dayv= document.createElement("div");
+       dayv.innerText=temp[i];
+       document.querySelector(`#${counter[i]}`).innerText="";
+       document.querySelector(`#${counter[i]}`).append(dayv,icon,value);
+   }
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
 }
 function currentlocation(data){
     
@@ -90,6 +149,6 @@ function currentlocation(data){
     var map=document.querySelector("iframe");
     map.src=`https://www.google.com/maps/embed/v1/place?key=AIzaSyBM7xfZ6ekcrWhf79KIju8kGADdjeHRSp4&q=${data.name}`;
     document.querySelector("#lcont").append(currentt,namediv,dataw );
- 
 
 }
+
